d8932a0ff65c3c6ca412dbab7e561ddd506e083d
mosobhy <mosobhy266@gmail.com>
2022-07-27 14:59:23 +0200

    client has been added

--- src/ClientSimulation.java
+++ src/ClientSimulation.java
@@ -1,2 +1,7 @@
 public class ClientSimulation {
+    // here we will have a client sending an http request to the
+    // we will have lots of options
+    // 1. send a single request
+    // 2. send bulk of requests
+    // 3. send thousands of requests
 }
